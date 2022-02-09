c3b1781ef3a221ccf484110545460d01dd88b186
Seamus English <seamus.english58@myhunter.cuny.edu>
2022-02-09 18:59:51 -0500

    added navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Navbar() {
+    return (<nav>
+        <ul>
+            <li><a href="default.asp">Home</a></li>
+            <li><a href="news.asp">News</a></li>
+            <li><a href="contact.asp">Contact</a></li>
+            <li><a href="about.asp">About</a></li>
+        </ul>
+    </nav>)
+}
+
+export default Navbar;
